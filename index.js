30c8d9f562cbc3202f99098c31afc58d66e86311
NoxusJr <ctt.georgejr@gmail.com>
2024-01-30 00:31:15 -0300

    Refatorando

--- index.js
+++ index.js
@@ -1,90 +1,116 @@
 // @ts-check
 const { exec } = require("child_process");
-const fs = require("fs");
-const { getSemanticVersion, parseVersions, useVersion, filterValidVersions } = require('./MODULES/versionManager')
+const fs = require("fs").promises;
+const { getSemanticVersion, parseVersions, useVersion, filterValidVersions } = require('./MODULES/versionManager');
 
 
-let packageJson = "";
 
-try {
-  packageJson = fs.readFileSync("package.json", "utf-8");
-} catch {
-  useVersion("current");
+async function readPackageJson() {
+  try {
+    const packageJson = await fs.readFile("package.json", "utf-8");
+    return JSON.parse(packageJson);
+  } catch (error) {
+    throw new Error("Error: Unable to read package.json");
+  }
 }
 
-const parsedPackageJson = JSON.parse(packageJson);
 
-const packageJsonNodeVersions = parsedPackageJson?.engines?.node
-  ?.split(/\|\||\s/)
-  .map((version) => version.trim().replace(".x", ""));
-
-if (!packageJsonNodeVersions?.length) {
-  useVersion("current");
-}
-
-const minimumVersion = getSemanticVersion(
-  packageJsonNodeVersions.find((version) => version.startsWith(">"))
-);
-
-const maximumVersion = getSemanticVersion(
-  packageJsonNodeVersions.find((version) => version.startsWith("<"))
-);
-
-if (!minimumVersion && !maximumVersion) {
-  const biggestVersion = packageJsonNodeVersions.reduce(
-    (acc, version) => (acc > version ? acc : version),
-    packageJsonNodeVersions[0]
+function findMinMaxVersions(versions) {
+  const minimumVersion = getSemanticVersion(
+    versions.find((version) => version.startsWith(">"))
   );
-  useVersion(biggestVersion);
+
+  const maximumVersion = getSemanticVersion(
+    versions.find((version) => version.startsWith("<"))
+  );
+
+  return { minimumVersion, maximumVersion };
 }
 
-const nvmSource = `source ${process.env.NVM_DIR}/nvm.sh`;
 
-exec(`${nvmSource} && nvm list`, (error, stdout) => {
-  if (error) {
-    console.log(error);
+async function main() {
+  let packageJson;
+  try {
+    packageJson = await readPackageJson();
+  } catch (error) {
+    console.error(error.message);
+    useVersion("current");
     return;
   }
 
-  const versions = parseVersions(stdout);
 
-  const filteredVersions = filterValidVersions(
-    versions,
-    minimumVersion,
-    maximumVersion
-  );
+  const packageJsonNodeVersions = packageJson?.engines?.node
+    ?.split(/\|\||\s/)
+    .map((version) => version.trim().replace(".x", ""));
 
-  if (filteredVersions.length) {
-    const latestVersion = filteredVersions.pop();
-    latestVersion && useVersion(latestVersion);
-  }
-});
-
-exec(`${nvmSource} && nvm ls-remote`, (error, stdout) => {
-  if (error) {
-    console.log(error);
+  if (!packageJsonNodeVersions?.length) {
+    useVersion("current");
     return;
   }
 
-  const versions = parseVersions(stdout);
-
-  const filteredVersions = filterValidVersions(
-    versions,
-    minimumVersion,
-    maximumVersion
+  const { minimumVersion, maximumVersion } = findMinMaxVersions(
+    packageJsonNodeVersions
   );
 
-  if (filteredVersions.length) {
-    const latestVersion = filteredVersions.pop();
-    exec(`${nvmSource} && nvm install ${latestVersion}`, (error) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
+  if (!minimumVersion && !maximumVersion) {
+    const biggestVersion = packageJsonNodeVersions.reduce(
+      (acc, version) => (acc > version ? acc : version),
+      packageJsonNodeVersions[0]
+    );
+    useVersion(biggestVersion);
+    return;
+  }
 
+  const nvmSource = `source ${process.env.NVM_DIR}/nvm.sh`;
+
+
+  exec(`${nvmSource} && nvm list`, async (error, stdout) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    const versions = parseVersions(stdout);
+
+    const filteredVersions = filterValidVersions(
+      versions,
+      minimumVersion,
+      maximumVersion
+    );
+
+    if (filteredVersions.length) {
+      const latestVersion = filteredVersions.pop();
       latestVersion && useVersion(latestVersion);
-    });
-  }
-});
+    }
+  });
 
-useVersion("current");
+
+  exec(`${nvmSource} && nvm ls-remote`, async (error, stdout) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    const versions = parseVersions(stdout);
+
+    const filteredVersions = filterValidVersions(
+      versions,
+      minimumVersion,
+      maximumVersion
+    );
+
+    if (filteredVersions.length) {
+      const latestVersion = filteredVersions.pop();
+      exec(`${nvmSource} && nvm install ${latestVersion}`, (error) => {
+        if (error) {
+          console.error(error);
+          return;
+        }
+
+        latestVersion && useVersion(latestVersion);
+      });
+    }
+  });
+}
+
+main();
